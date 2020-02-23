7974a155ee12ffb4e128144b52d6ed4d387979fa
StevenElbery <stevenelbery@gmail.com>
2020-02-23 11:20:58 -0500

    DELETE enpoints created

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ APP = create_app()
 #Get movies from casting_agency database
 
 @app.route('/movies')
-@requires_auth('get:movies)
+@requires_auth('get:movies')
 def retrieve_movies():
   try:
       movies = Movie.query.all()
@@ -34,8 +34,10 @@ def retrieve_movies():
 
 #Get actors from casting_agency database
 
+
+
 @app.route('/actors')
-@requires_auth('get:actors)
+@requires_auth('get:actors')
 def retrieve_actors():
   try:
       actors = Actor.query.all()
@@ -50,6 +52,48 @@ def retrieve_actors():
   except:
     abort(422)
 
+
+#Delete movies from casting_agency database
+
+@app.route('/movies/<int:movie_id>', methods=['DELETE'])
+@requires_auth('delete:movies')
+def delete_movie(movie_id):
+    try:
+      movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+      if movie is None:
+        abort(404)
+
+      movie.delete()
+
+      return jsonify({
+        'success': True,
+        'delete': movie_id
+      })
+
+    except:
+      abort(422)
+
+#Delete actors from casting_agency database
+
+@app.route('/actors/<int:actor_id>', methods=['DELETE'])
+@requires_auth('delete:actors')
+def delete_actor(actor_id):
+    try:
+      actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+      if actor is None:
+        abort(404)
+
+      actor.delete()
+
+      return jsonify({
+        'success': True,
+        'delete': actor_id
+      })
+
+    except:
+      abort(422)
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
